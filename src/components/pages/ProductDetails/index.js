aea68e89df8a5a262e8d841918be772592f3c919
Leandro-Suero <ing.leandrosuero@gmail.com>
2020-12-06 23:22:06 -0300

    logic for product detail info page

--- src/components/pages/ProductDetails/index.js
+++ src/components/pages/ProductDetails/index.js
@@ -1,14 +1,46 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
-function ProductDetails({ product }) {
+import Spinner from "../../Spinner";
+
+function ProductDetails() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [product, setProduct] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    setError("");
+    axios
+      .get(`/api/items/${id}`)
+      .then(({ data: { item } }) => {
+        setProduct(item);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setError("Hubo un error");
+        setLoading(false);
+      });
+  }, [id]);
+
   return (
+    error !== "" ? 'Hubo un error' : 
+    loading ? <Spinner /> : 
     <div>
-      <h1>{product.title}</h1>
+      <p>{product.picture}</p>
+      <p>{product.condition}</p>
+      <p>{product.sold_quantity}</p>
+      <p>{product.title}</p>
+      <p>{product.price.amount}<span>.{product.price.decimals}</span></p>
+      <p>{product.free_shipping}</p>
+      <button>Comprar</button>
+      <h3>Descripción</h3>
+      <p>{product.description}</p>
     </div>
   );
 }
-
-ProductDetails.propTypes = {};
 
 export default ProductDetails;
