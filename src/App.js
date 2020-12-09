58a891aa7a01d26ae695fb64b890ad0781cf8e15
Leandro-Suero <ing.leandrosuero@gmail.com>
2020-12-09 00:33:13 -0300

    Code splitting separating SearchResults, ProductDetails, NotFound

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import axios from "axios";
 import "./App.scss";
 
 import Home from "./components/pages/Home";
-import SearchResults from "./components/pages/SearchResults";
-import ProductDetails from "./components/pages/ProductDetails";
-import NotFound from "./components/pages/NotFound";
 import SearchBox from "./components/SearchBox";
+import Spinner from "./components/Spinner";
+
+const SearchResults = lazy(() => import("./components/pages/SearchResults"));
+const ProductDetails = lazy(() => import("./components/pages/ProductDetails"));
+const NotFound = lazy(() => import("./components/pages/NotFound"));
 
 function App() {
   //default domain for all future axios calls
@@ -18,17 +20,24 @@ function App() {
     <div className="App">
       <HelmetProvider>
         <Helmet>
-              <title>Mercado Libre Argentina - Donde comprar y vender de todo</title>
-              <meta name="description" content="El mas grande mercado online de la region, mas rápido y seguro! De la tienda del vendedor a tu casa de la manera mas practica y facil." />
+          <title>
+            Mercado Libre Argentina - Donde comprar y vender de todo
+          </title>
+          <meta
+            name="description"
+            content="El mas grande mercado online de la region, mas rápido y seguro! De la tienda del vendedor a tu casa de la manera mas practica y facil."
+          />
         </Helmet>
         <Router>
           <SearchBox />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/items" component={SearchResults} />
-            <Route exact path="/items/:id" component={ProductDetails} />
-            <Route component={NotFound} />
-          </Switch>
+          <Suspense fallback={<Spinner />}>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/items" component={SearchResults} />
+              <Route exact path="/items/:id" component={ProductDetails} />
+              <Route component={NotFound} />
+            </Switch>
+          </Suspense>
         </Router>
       </HelmetProvider>
     </div>
