47f6961b257a8988818660c13236c8dcf514038d
Leandro-Suero <ing.leandrosuero@gmail.com>
2020-12-09 11:23:41 -0300

    hidding description in product detail view when there is no description

--- src/components/pages/ProductDetails/index.js
+++ src/components/pages/ProductDetails/index.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import axios from "axios";
-import './index.scss';
+import "./index.scss";
 
 import Breadcrumbs from "../../Breadcrumbs";
 import ShowError from "../../ShowError";
 import Spinner from "../../Spinner";
 import Price from "../../Price";
-import { ProductConditionConverter } from '../../../libs/Converter'
+import { ProductConditionConverter } from "../../../libs/Converter";
 
 function ProductDetails() {
   const [loading, setLoading] = useState(true);
@@ -32,21 +32,38 @@ function ProductDetails() {
       });
   }, [id]);
 
-  return (
-    error !== "" ? <ShowError message={error} /> : 
-    loading ? <Spinner /> : 
+  return error !== "" ? (
+    <ShowError message={error} />
+  ) : loading ? (
+    <Spinner />
+  ) : (
     <>
       <Helmet>
         <title>{`Mercado Libre - ${id} - ${product.title}`}</title>
-        <meta name="description" content={`Todo sobre ${product.title}, fotos, cantidad disponible, estado, compra ahora mismo para recibirlo en tu casa!`} />
+        <meta
+          name="description"
+          content={`Todo sobre ${product.title}, fotos, cantidad disponible, estado, compra ahora mismo para recibirlo en tu casa!`}
+        />
       </Helmet>
-      <Breadcrumbs data={["HOWTO","Agregar category_id al API","Fetch /categories/category_id","Usar path_from_root", "Construir Breadcrumbs"]} />
+      <Breadcrumbs
+        data={[
+          "HOWTO",
+          "Agregar category_id al API",
+          "Fetch /categories/category_id",
+          "Usar path_from_root",
+          "Construir Breadcrumbs",
+        ]}
+      />
       <section className="ui-pdp-container">
         <div className="ui-pdp-content">
           <div className="ui-pdp-content__row">
             <div className="ui-pdp__col-main">
               <div className="ui-pdp-image-container">
-                <img className="ui-pdp-image" src={product.picture} alt="Producto"/>
+                <img
+                  className="ui-pdp-image"
+                  src={product.picture}
+                  alt="Producto"
+                />
               </div>
             </div>
             <div className="ui-pdp__col-side">
@@ -55,29 +72,35 @@ function ProductDetails() {
                   <span className="ui-pdp-subtitle">
                     <span className="ui-pdp-subtitle__condition">
                       {ProductConditionConverter(product.condition)}
-                    </span> - {product.sold_quantity} vendidos
+                    </span>{" "}
+                    - {product.sold_quantity} vendidos
                   </span>
                 </div>
-                <h1 className="ui-pdp-title">
-                  {product.title}
-                </h1>
+                <h1 className="ui-pdp-title">{product.title}</h1>
               </div>
-              <Price price={product.price} free_shipping={product.free_shipping} />
+              <Price
+                price={product.price}
+                free_shipping={product.free_shipping}
+              />
               <div className="ui-pdp-actions">
-                <button className="ui-pdp-actions__cta">Comprar</button>  
+                <button className="ui-pdp-actions__cta">Comprar</button>
               </div>
             </div>
           </div>
-          <div className="ui-pdp-content__row">
-            <div className="ui-pdp__col-main">
-              <div className="ui-pdp-description">
-                <h2 className="ui-pdp-description__title">
-                  Descripción del producto
-                </h2>
-                <p className="ui-pdp-description__content">{product.description}</p>
+          {product.description !== "" && (
+            <div className="ui-pdp-content__row">
+              <div className="ui-pdp__col-main">
+                <div className="ui-pdp-description">
+                  <h2 className="ui-pdp-description__title">
+                    Descripción del producto
+                  </h2>
+                  <p className="ui-pdp-description__content">
+                    {product.description}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
       </section>
     </>
