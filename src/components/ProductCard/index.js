0bfcbde5613eb99c8c7e2b38753482e6e769d800
Leandro-Suero <ing.leandrosuero@gmail.com>
2020-12-10 15:23:46 -0300

    adding seller city to the product card in the search results

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -36,9 +36,7 @@ function ProductCard({ product }) {
           {/* Según la documentación de la API solicitada, no está el dato de "Localidad" para mostrar
             aquí como se observa en las specs, por lo tanto muestro en su lugar el "condition" (que no 
              era visible en esta pantalla) para respetar el diseño visualmente */}
-          <span className="ui-pcard-condition">
-            {ProductConditionConverter(product.condition)}
-          </span>
+          <span className="ui-pcard-city">{product.seller_city_name}</span>
         </div>
       </div>
     </div>
